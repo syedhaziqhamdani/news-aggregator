0a60bfd43c053f7341e3e6a494b51b75eba56c24
Syed Haziq Hamdani <syedhaziqhamdani@gmail.com>
2024-11-17 16:20:52 +0500

    feat: update Article model with fillable attributes
    - Added `fillable` properties to allow mass assignment for:
      - `title`, `description`, `url`, `source`, and `category`
    - Ensured compatibility with Laravel's factory functionality

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -2,9 +2,19 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Article extends Model
 {
     //
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'url',
+        'source',
+        'category',
+    ];
 }
